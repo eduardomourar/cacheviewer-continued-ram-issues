ac3aa6c2c8c8186c36f2c19800a55ba330b53402
James Sumners <james.sumners@gmail.com>
2011-12-04 19:13:19 -0500

    Fix the loading of translations as per bug report #10.

--- defaults/preferences/pref.js
+++ defaults/preferences/pref.js
@@ -2,4 +2,4 @@ pref("extensions.cacheviewer.open_in_separate_window", true);
 pref("extensions.cacheviewer.folder", "");
 pref("extensions.cacheviewer.warn_on_open", true);
 pref("extensions.cacheviewer.max_open_before_warn", 15);
-pref("extensions.{71328583-3CA7-4809-B4BA-570A85818FBB}.description", "chrome://cacheviewer/locale/cacheviewer.properties");
+pref("extensions.{30E08C68-889E-11E0-95EF-DA7E4824019B}.description", "chrome://cacheviewer/locale/cacheviewer.properties");
